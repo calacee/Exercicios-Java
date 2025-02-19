0ec967b0a35e4610e1fbbece0fe9509898f62565
felipecalace <felipecalacemiranda@gmail.com>
2025-02-19 12:39:26 -0300

    correção

--- Votacao.java
+++ Votacao.java
@@ -4,17 +4,6 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Votacao {
     public static void main(String[] args) {
-//        8) Votação
-//        Crie uma classe chamada Votacao: Foi feita uma pesquisa para saber
-//        qual o sabor de pizza favorito entre os alunos da Faculdade:
-//
-//        Para votar no sabor mussarela, sorteie o valor 1.
-//        Para votar no sabor calabresa, sorteie o valor 2.
-//        Para votar no sabor quatro queijos, sorteie o valor 3.
-//        Sorteie os votos de 10 alunos diferentes (sem usar vetor) e exiba:
-//
-//        Quantos votos obteve cada sabor.
-//                Qual o sabor favorito segundo a pesquisa.
         Integer votosMussarela = 0;
         Integer votosCalabresa = 0;
         Integer votos4Queijos = 0;
@@ -33,7 +22,13 @@ public class Votacao {
             System.out.println("Calabresa é favorita");
         else if (votosMussarela > votosCalabresa && votosMussarela > votos4Queijos)
             System.out.println("Mussarela é favorita");
-        else
+        else if (votos4Queijos > votosMussarela && votos4Queijos > votosCalabresa)
             System.out.println("Quatro Queijos é favorita");
+        else if (votosCalabresa == votos4Queijos)
+            System.out.println("Calabresa e 4 Queijos empataram");
+        else if (votosCalabresa == votosMussarela)
+            System.out.println("Calabresa e Mussarela empataram");
+        else
+            System.out.println("Mussarela e 4 queijos empataram");
     }
 }
